0b24c9ba6eb756cacc03cc2ac5984524d9937235
Tony Lopar <tonylopar@hotmail.com>
2021-12-02 20:20:44 +0100

    Advent of code setup + day 1

---
+++ src/main/kotlin/day1/day1.kt
@@ -0,0 +1,45 @@
+package day1
+
+import java.io.File
+
+fun main(args: Array<String>) {
+    val lines = readLinesFromFile("day1");
+    val measurements = lines.map { it.toInt() }
+
+    println("Part 1:");
+    println(countIncreasedMeasurementsPart1(measurements));
+
+    println("Part 2:");
+    println(countIncreasedMeasurementsPart2(measurements));
+}
+
+fun readLinesFromFile(dayName: String): List<String>
+        = File("src/main/kotlin/$dayName", "$dayName.txt").readLines()
+
+fun countIncreasedMeasurementsPart1(measurements: List<Int>):Int {
+    var higherMeasurements = 0;
+    var previousMeasurement = 0;
+    for ((index, measurement) in measurements.withIndex()) {
+        if (index > 0 && measurement > previousMeasurement) {
+            higherMeasurements++;
+        }
+        previousMeasurement = measurement;
+    }
+    return higherMeasurements;
+}
+
+fun countIncreasedMeasurementsPart2(measurements: List<Int>):Int {
+    var higherMeasurements = 0;
+    var previousMeasurement = 0;
+
+    for (index in measurements.indices) {
+        if (index + 3 > measurements.count()) break;
+
+        val windowSum = measurements.subList(index, index + 3).sum();
+        if (index > 0 && windowSum > previousMeasurement) {
+            higherMeasurements++;
+        }
+        previousMeasurement = windowSum;
+    }
+    return higherMeasurements;
+}
